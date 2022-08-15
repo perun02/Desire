3eb85fcbd729c111e24b0f821175abd474c0cda7
perun02 <lesha.chey@yandex.ru>
2022-08-15 15:58:05 +0600

    release

---
+++ gulp-start/app/js/main.js
@@ -0,0 +1,36 @@
+$(function () {
+
+    $('.header__btn').on('click', function () {
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
+    $('.rightside-menu__close').on('click', function () {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    })
+
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+    });
+
+    $('.contact-slider').slick({
+        slidesToShow: 10,
+        slidesToScroll: 10,
+        dots: true,
+        arrows: false,
+        autoplay: true,
+    });
+
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slick-left.svg" alt="arrow left"></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slick-right.svg" alt="arrow right"></button>',
+    });
+
+    var mixer = mixitup('.gallery__inner', {
+        load: {
+            filter: '.living'
+        }
+    });
+
+})
